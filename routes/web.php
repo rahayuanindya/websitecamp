66476a0ec8e4c10d383c49ac308d7148df62ea5b
heryprayitno93@gmail.com <heryprayitno93@gmail.com>
2023-12-27 16:03:52 +0700

    New Midtrans Callback

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProvideHandle'])->name('user.google.callback');
 
+//midtrans routes
+Route::get('payment/success', [UserController::class, 'midtransCallback']);
+Route::post('payment/success', [UserController::class, 'midtransCallback']);
+
 Route::middleware(['auth'])->group(function (){
     Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success')->middleware('ensureUserRole:user');
     Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create')->middleware('ensureUserRole:user');
